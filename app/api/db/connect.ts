02aa4bca8a2456bc16872bc9c2942b6705d6e11a
okazakee <okazakee@proton.me>
2024-11-07 00:43:38 +0100

    experimenting with supabase js lib

---
+++ app/api/db/connect.ts
@@ -0,0 +1,27 @@
+'use server'
+import { createClient, SupabaseClient } from '@supabase/supabase-js'
+import type { Database } from '@/database.types';
+
+const supabaseUrl = process.env.SUPABASE_URL as string;
+const supabaseKey = process.env.SUPABASE_ANON_KEY as string;
+
+const supabase: SupabaseClient = createClient<Database>(supabaseUrl, supabaseKey);
+
+export const fetchBio = async () => {
+
+  const { data, error } = await supabase
+  .from('biography')
+  .select('propic, aboutmedesc, headerdesc')
+
+  console.log(data![0])
+
+  if (error) {
+
+    console.error('Error fetching data:', error)
+
+  } else {
+
+    return data[0]
+
+  }
+}
--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,6 +1,1 @@
-import { createClient, SupabaseClient } from '@supabase/supabase-js'
-
-const supabaseUrl = process.env.SUPABASE_URL as string;
-const supabaseKey = process.env.SUPABASE_ANON_KEY as string;
-
-export const supabase: SupabaseClient = createClient(supabaseUrl, supabaseKey)
+//not useful for now
