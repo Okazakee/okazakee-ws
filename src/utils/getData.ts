f9c4d08a4b5f9e6a460f0f8c1108e54ff64263ca
okazakee <okazakee@proton.me>
2025-04-11 09:53:38 +0200

    test build

--- src/utils/getData.ts
+++ src/utils/getData.ts
@@ -17,7 +17,7 @@ export const supabase = createClient(supabaseUrl || '', supabaseKey || '');
 
 const production = JSON.parse(process.env.UMAMI_ENABLED || 'false');
 
-const revalTime = production ? 86400 : 60;
+const revalTime = production ? 60 : 60;
 
 const getCurrentTime = () => new Date().toISOString();
 
